637f9524afbb1610203c8d810f1e92fc035a4c79
BrunoJun <brunojun7@gmail.com>
2021-10-01 14:48:47 -0300

    Initial commit

---
+++ projetoAPPCRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoAPPCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
