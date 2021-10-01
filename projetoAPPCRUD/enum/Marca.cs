038f496f06a406e11fb05d5933f8348eb3e70f19
BrunoJun <brunojun7@gmail.com>
2021-10-01 15:28:33 -0300

    Create Enum Marca

---
+++ projetoAPPCRUD/enum/Marca.cs
@@ -0,0 +1,17 @@
+namespace projetoAPPCRUD
+{
+    public enum Marca
+    {
+        
+        Toyota = 1,
+        Volkswagen = 2,
+        Ford = 3,
+        Honda = 4,
+        Hyundai = 5,
+        Nissan = 6,
+        Chevrolet = 7,
+        Kia = 8,
+        Mercedes = 9,
+        BMW = 10,
+    }
+}
